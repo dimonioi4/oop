d4e313dd4b9ae55444a0dca7a5bacfb7c37f476f
dimonioi4 <dimonioi4@yandex.ru>
2021-03-18 22:28:57 +0300

    update VK

---
+++ src/main/java/domain/Donut.java
@@ -0,0 +1,7 @@
+package domain;
+
+public class Donut {
+    private boolean Donut;
+    private int paid_duration;
+    private boolean can_publish_free_copy;
+}
---
+++ src/main/java/domain/Geo.java
@@ -0,0 +1,6 @@
+package domain;
+
+public class Geo {
+    private String type;
+    private String coordinates;
+}
