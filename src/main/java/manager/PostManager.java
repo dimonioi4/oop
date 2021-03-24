88d62b3973ac70667ac05835bba804606594d850
dimonioi4 <dimonioi4@yandex.ru>
2021-03-24 23:25:37 +0300

    add PostManager

---
+++ src/main/java/manager/PostManager.java
@@ -0,0 +1,14 @@
+package manager;
+import domain.Post;
+
+public class PostManager {
+    private Post[] post;
+    public Post[] search(String ownerId,String  domain, String query, boolean ownerOnly, int count, int offset) {
+        //TODO: add logic
+        return null;
+    }
+    public void delete(String ownerId, String postId) {
+        //TODO: add logic
+    }
+}
+
